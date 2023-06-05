68d3544effd390423c8646973936f7e0c19ebd48
Voloshun555 <voloshunihor@gmail.com>
2023-06-05 15:35:33 +0300

    789

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,15 +5,14 @@ import { Statistics } from 'components/Statistics/Statistics';
 import { Notification } from 'components/Notification/Notification ';
 
 function App() {
-  const  [good, setGood ] = useState(0);
-  const  [neutral, setNeutral]  = useState(0);
-  const  [bad, setBad]  = useState(0);
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   const total = good + neutral + bad;
   const feedbackInPercentage = Math.round((good / total) * 100);
-  
-  function onLeaveFeedback(evt) {
 
+  function onLeaveFeedback(evt) {
     switch (evt) {
       case 'good':
         setGood(good + 1);
@@ -30,28 +29,28 @@ function App() {
     }
   }
 
-return (
-  <section>
-    <Section title={'Please leave feedback'}>
-      <Feedback
-        options={['good', 'neutral', 'bad']}
-        onLeaveFeedback={onLeaveFeedback}
-      />
-    </Section>
-    <Section title={'Statistics'}>
-      {total > 0 ? (
-        <Statistics
-          good={good}
-          neutral={neutral}
-          bad={bad}
-          total={feedbackInPercentage}
-          positivFeedback={feedbackInPercentage}
+  return (
+    <section>
+      <Section title={'Please leave feedback'}>
+        <Feedback
+          options={Object.keys({ good, neutral, bad })}
+          onLeaveFeedback={onLeaveFeedback}
         />
-      ) : (
-        <Notification name="There is no feedback" />
-      )}
-    </Section>
-  </section>
-);
+      </Section>
+      <Section title={'Statistics'}>
+        {total > 0 ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={feedbackInPercentage}
+            positivFeedback={feedbackInPercentage}
+          />
+        ) : (
+          <Notification name="There is no feedback" />
+        )}
+      </Section>
+    </section>
+  );
 }
 export default App;
